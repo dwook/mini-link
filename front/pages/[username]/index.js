67ec435623bb72a445bfbeaac6b8d3b69b12c2c2
dwook <igerapex@gmail.com>
2021-01-09 12:37:17 +0900

    Feature getting ip from browser

--- front/pages/[username]/index.js
+++ front/pages/[username]/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import styled from 'styled-components';
@@ -26,40 +26,78 @@ const Content = styled.div`
   }
 `;
 
-const MiniHome = ({ miniHome, miniLinks, username }) => (
-  <>
-    <Head>
-      <title>{username} 미니링크</title>
-      <meta property="og:title" content={`${username} 미니링크`} />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={`${backURL}/${username}`} />
-      <meta property="og:image" content={`${backURL}/${miniHome.coverImage}`} />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <Header mainColor={miniHome.mainColor}>
-      <CoverImage imageURL={`${backURL}/${miniHome.coverImage}`}>
-        <CoverButtonList>
-          <CoverButton outsite icon={<Home />} href={miniHome.website} />
-          <CoverButton outsite icon={<Instagram />} href={miniHome.instagram} />
-          <CoverButton outsite icon={<Youtube />} href={miniHome.youtube} />
-          <CoverButton icon={<Share />} />
-        </CoverButtonList>
-      </CoverImage>
-    </Header>
-    <Content>
-      {miniLinks &&
-        miniLinks.map((link) => (
-          <a href={link.url} target="_blank" rel="noopener noreferrer">
-            <MiniLink key={link.id} imageURL={`'${backURL}/${link.image}'`}>
-              <div className="content">
-                <p className="title">{link.name}</p>
-              </div>
-            </MiniLink>
-          </a>
-        ))}
-    </Content>
-  </>
-);
+const MiniHome = ({ miniHome, miniLinks, username }) => {
+  const [ip, setIp] = useState(null);
+
+  const onClickHandler = useCallback(
+    (linkId) => () => {
+      axios.post(`${backURL}/visit?linkId=${linkId}&ip=${ip}`);
+    },
+    []
+  );
+
+  useEffect(async () => {
+    const data = await fetch('https://api.ipify.org?format=json', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => res.ip);
+    setIp(data);
+    if (data) {
+      axios.post(`${backURL}/visit?homeId=${miniHome.id}&ip=${ip}`);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>{username} 미니링크</title>
+        <meta property="og:title" content={`${username} 미니링크`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`${backURL}/${username}`} />
+        <meta
+          property="og:image"
+          content={`${backURL}/${miniHome.coverImage}`}
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Header mainColor={miniHome.mainColor}>
+        <CoverImage imageURL={`${backURL}/${miniHome.coverImage}`}>
+          <CoverButtonList>
+            <CoverButton outsite icon={<Home />} href={miniHome.website} />
+            <CoverButton
+              outsite
+              icon={<Instagram />}
+              href={miniHome.instagram}
+            />
+            <CoverButton outsite icon={<Youtube />} href={miniHome.youtube} />
+            <CoverButton icon={<Share />} />
+          </CoverButtonList>
+        </CoverImage>
+      </Header>
+      <Content>
+        {miniLinks &&
+          miniLinks.map((link) => (
+            <a
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={link.id}
+            >
+              <MiniLink
+                imageURL={`'${backURL}/${link.image}'`}
+                onClick={onClickHandler(link.id)}
+              >
+                <div className="content">
+                  <p className="title">{link.name}</p>
+                </div>
+              </MiniLink>
+            </a>
+          ))}
+      </Content>
+    </>
+  );
+};
 
 MiniHome.propTypes = {
   miniHome: PropTypes.object.isRequired,
