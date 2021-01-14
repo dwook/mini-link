f1636e8ef16cb579ed74df7b4bb72b9d5325a691
dwook <igerapex@gmail.com>
2021-01-14 15:45:46 +0900

    Add viewport setting

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import wrapper from '../store';
@@ -7,6 +8,12 @@ import theme from '../src/style/theme';
 
 const App = ({ Component, pageProps }) => (
   <>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, height=device-height, initial-scale=1, user-scalable=no"
+      />
+    </Head>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
