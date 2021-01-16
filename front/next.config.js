619bb2267579140438c8e72a3b8423ccb406a01d
DongWook <igerapex@gmail.com>
2021-01-16 14:44:43 +0900

    Add bundle-analyzer

---
+++ front/next.config.js
@@ -0,0 +1,21 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    const newConfig = {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      plugins: [
+        ...config.plugins,
+      ],
+    };
+    if (prod) {
+      newConfig.devtool = 'hidden-source-map';
+    }
+    return newConfig;
+  },
+});
