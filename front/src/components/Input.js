c8b680147e09a284739453f0eb0d37f9b2a5941f
dwook <igerapex@gmail.com>
2021-01-18 10:11:11 +0900

    Change input font size

--- front/src/components/Input.js
+++ front/src/components/Input.js
@@ -13,6 +13,7 @@ const Input = styled.input`
   color: #333;
   border: 0px;
   border-radius: 6px;
+  font-size: 14px;
   background: rgb(255, 255, 255);
   box-shadow: rgba(50, 50, 93, 0.2) 0px 0px 0px 1px,
     rgba(50, 50, 93, 0.1) 0px 2px 5px 0px, rgba(0, 0, 0, 0.1) 0px 1px 1px 0px;
